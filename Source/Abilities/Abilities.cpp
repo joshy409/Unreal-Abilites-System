b105977061dad1eceea674f29f1edb81c1f342f9
Joshua Yang <ybd1992@hotmail.com>
2018-11-07 14:59:34 -0800

    Initial commit

---
+++ Source/Abilities/Abilities.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "Abilities.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Abilities, "Abilities" );
+ 
